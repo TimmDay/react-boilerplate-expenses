95dd2408a5f9ecb27969f7f2499c0293bbf2a5e9
TimmDay <timmday.info@gmail.com>
2018-05-28 18:40:07 +0200

    set up production build and server - heroku

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express(); //create new instance
 const publicPath = path.join(__dirname, '..', 'public');
 
+const port = process.env.PORT || 3000; //heroku sets this PORT value
+
 // where files live
 // what port to listen on
 
@@ -18,7 +20,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-// start up on port 3000
-app.listen(3000, () => {
-    console.log('Express server is up');
-}); //listen to this port
+// start up on port 3000 for local machine
+// app.listen(3000, () => {
+//     console.log('Express server is up');
+// }); //listen to this port
+
+// for HEROKU
+app.listen(port, () => {
+    console.log('heroku server is up');
+});
+
+//teach heroku how to run webpack
