89fdac5781c97c8b851f50cab9ee8e008d1038c0
Timmday <timmday.info@gmail.com>
2019-02-12 12:22:07 +0100

    update typos

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,10 +4,7 @@ import expensesReducer from '../reducers/expenses';
 import filtersReducer from './../reducers/filters';
 import authReducer from './../reducers/auth';
 
-
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const composeEnhancers = compose;
 
 export default () => {
 
@@ -17,9 +14,7 @@ export default () => {
             filters: filtersReducer,
             auth: authReducer
         }),
-        // applyMiddleware(thunk)
         composeEnhancers(applyMiddleware(thunk))
-        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
     return store;
@@ -27,5 +22,3 @@ export default () => {
 
 // STORE creation
 // tracks the state (the thing returned when an action is processed by the reducer
-
-
